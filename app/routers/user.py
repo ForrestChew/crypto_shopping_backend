cf61d9bacd4fd8943f409778594084b57d9686e5
Forrest Chew <forrestchew@gmail.com>
2022-09-29 13:58:47 -0700

    Added delete user API Endpoint

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,6 +1,6 @@
-from fastapi import status, HTTPException, Depends, APIRouter
+from fastapi import status, HTTPException, Depends, APIRouter, Response
 from sqlalchemy.orm import Session
-from .. import schemas, models, utils
+from .. import schemas, models, utils, oauth2
 from ..database import get_db
 
 
@@ -18,3 +18,29 @@ def create_user(user: schemas.CreateUser, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@router.delete("/{id}")
+def delete_user(
+    id: int,
+    current_user: str = Depends(oauth2.get_current_user),
+    db: Session = Depends(get_db),
+):
+    user_query = db.query(models.User).filter(models.User.id == id)
+    user = user_query.first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id {id} was not found",
+        )
+    print("Queried ID", id)
+    print("User ID", user.id)
+    print("Current User ID", current_user.id)
+    if user.id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not authorized to perform action.",
+        )
+    user_query.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
