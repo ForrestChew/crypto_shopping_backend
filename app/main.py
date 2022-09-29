967cdcedce1d37d0d4a0457acff9e361b5b67f87
Forrest Chew <forrestchew@gmail.com>
2022-09-29 14:01:54 -0700

    Added auth routes to main module

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from .routers import user
+from .routers import auth, user
 
 app = FastAPI()
 
 app.include_router(user.router)
+app.include_router(auth.router)
 
 
 @app.get("/")
