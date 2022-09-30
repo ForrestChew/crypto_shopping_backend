690e2599a1bd0a3b5bb8b6b11a8ef7cd19693813
Forrest Chew <forrestchew@gmail.com>
2022-09-30 15:10:33 -0700

    Created /carts/ path

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from .routers import auth, user, product
+from .routers import auth, user, product, cart
 
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(product.router)
+app.include_router(cart.router)
 
 
 @app.get("/")
---
+++ app/routers/cart.py
@@ -0,0 +1,16 @@
+from fastapi import status, HTTPException, Depends, APIRouter
+from sqlalchemy.orm import Session
+from .. import schemas, models
+from ..database import get_db
+
+router = APIRouter(prefix="/carts", tags=["Carts"])
+
+
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.CreatedCart)
+def get_cart_by_id(id: int, db: Session = Depends(get_db)):
+    cart = db.query(models.Cart).filter(models.Cart.id == str(id)).first()
+    if not cart:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Cart not found"
+        )
+    return cart
