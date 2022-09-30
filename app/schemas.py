b22858ca626e04c93bf692f65af7757e0b5469ca
Forrest Chew <forrestchew@gmail.com>
2022-09-30 10:51:24 -0700

    Added CreateProduct and CreatedProduct pydantic schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,10 +1,12 @@
-from pydantic import BaseModel, EmailStr
+from xmlrpc.client import Boolean
+from pydantic import BaseModel, EmailStr, PositiveFloat
 from typing import Optional
 
 
 class CreateUser(BaseModel):
     email: EmailStr
     password: str
+    is_administrator: Optional[Boolean]
 
 
 class CreatedUser(BaseModel):
@@ -22,3 +24,17 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[str] = None
+
+
+class CreateProduct(BaseModel):
+    title: str
+    category: str
+    price: PositiveFloat
+    quantity: int
+
+
+class CreatedProduct(CreateProduct):
+    id: int
+
+    class Config:
+        orm_mode = True
