4ea621379c22212bf12241b66efeaa32bef9efa3
Forrest Chew <forrestchew@gmail.com>
2022-09-29 18:55:38 -0700

    Added products API module to entry point

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from .routers import auth, user
+from .routers import auth, user, product
 
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(product.router)
 
 
 @app.get("/")
