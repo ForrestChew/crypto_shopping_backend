2174624bc9c7f8507247f2aa43731aa79921aca6
Forrest Chew <forrestchew@gmail.com>
2022-10-12 14:52:53 -0700

    Enabled fastapi jwt extension to help with user authentication.

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 from pydantic import BaseSettings
+from fastapi_jwt_auth import AuthJWT
 
 
 class Settings(BaseSettings):
@@ -6,12 +7,21 @@ class Settings(BaseSettings):
     database_name: str
     database_username: str
     database_password: str
-    jose_jwt_secret_key: str
-    algorithm: str
-    access_token_expire_minutes: str
+    authjwt_secret_key: str
+    authjwt_token_location: set = {"cookies"}
+    authjwt_cookie_secure: bool
+    authjwt_cookie_csrf_protect: bool
+    authjwt_cookie_domain: str
+    authjwt_cookie_samesite: str
+    authjwt_cookie_max_age: int
 
     class Config:
         env_file = ".env"
 
 
 settings = Settings()
+
+
+@AuthJWT.load_config
+def get_config():
+    return settings
