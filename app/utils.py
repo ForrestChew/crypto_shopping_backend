c8076ae42a20e85344459b4644e8ce5af9c743d4
Forrest Chew <forrestchew@gmail.com>
2022-10-12 14:53:34 -0700

    Refactored for readablility

--- app/utils.py
+++ app/utils.py
@@ -1,11 +1,31 @@
 from passlib.context import CryptContext
+from fastapi import Depends, status, HTTPException
+from fastapi_jwt_auth import AuthJWT
+from requests import Session
+from app import models, database
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str):
     return password_context.hash(password)
 
 
-def verify_password(non_hashed_password: str, hashed_password: str) -> str:
+def verify_password(non_hashed_password: str, hashed_password: str):
     return password_context.verify(non_hashed_password, hashed_password)
+
+
+def get_user_id_from_jwt(
+    Authorize: AuthJWT = Depends(),
+    db: Session = Depends(database.get_db),
+):
+    try:
+        Authorize.jwt_required()
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Access token non-existant or not",
+        )
+    current_user_id = Authorize.get_jwt_subject()
+    user = db.query(models.User).filter(models.User.id == current_user_id).first()
+    return user
