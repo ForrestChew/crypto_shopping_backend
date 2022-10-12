6158b161fb52516d57c45891bb1748d84d4c06e8
Forrest Chew <forrestchew@gmail.com>
2022-10-12 14:55:09 -0700

    Added ability to set cookies in browser

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,16 +1,20 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+from fastapi_jwt_auth import AuthJWT
 from ..database import get_db
-from .. import schemas, models, utils, oauth2
+from .. import models, utils
 
-router = APIRouter(tags=["Authentication"])
+router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 
-@router.post("/login", response_model=schemas.Token)
+@router.post(
+    "/login",
+)
 def login(
     user_credentials: OAuth2PasswordRequestForm = Depends(),
     db: Session = Depends(get_db),
+    Authorize: AuthJWT = Depends(),
 ):
     user = (
         db.query(models.User)
@@ -25,5 +29,17 @@ def login(
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN, detail="Invalid credentials"
         )
-    access_token = oauth2.create_access_token(data={"user_id": user.id})
-    return {"access_token": access_token, "token_type": "bearer"}
+    access_token = Authorize.create_access_token(subject=str(user.id))
+    refresh_token = Authorize.create_refresh_token(subject=str(user.id))
+    Authorize.set_access_cookies(access_token)
+    Authorize.set_refresh_cookies(refresh_token)
+    return {"access_token": access_token, "refresh_token": refresh_token}
+
+
+@router.post("/refresh")
+def refresh(Authorize: AuthJWT = Depends()):
+    Authorize.jwt_refresh_token_required()
+    current_user_id = Authorize.get_jwt_subject()
+    new_access_token = Authorize.create_access_token(subject=current_user_id)
+    Authorize.set_access_cookies(new_access_token)
+    return {"access_token": new_access_token}
