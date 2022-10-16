313aaa582fb2974988230d69e15289dbd2e5402f
Forrest Chew <forrestchew@gmail.com>
2022-10-16 12:28:22 -0700

    Corrected import after module refactoring

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import auth, user, product, cart
+
+from .routers.product import product
+from .routers import auth, user, cart
 
 app = FastAPI()
 
