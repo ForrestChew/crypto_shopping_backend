5fa82c6868738c4d2db6c3a952b89b04e9b0982f
Forrest Chew <forrestchew@gmail.com>
2022-10-18 15:18:08 -0700

    added top deal getter endpoint

--- app/routers/product/product.py
+++ app/routers/product/product.py
@@ -1,5 +1,6 @@
-from symbol import test_nocond
+from unicodedata import name
 from fastapi import status, HTTPException, Depends, APIRouter
+from fastapi.responses import FileResponse
 from fastapi_jwt_auth import AuthJWT
 from typing import List, Optional
 from ... import models, schemas
@@ -9,6 +10,12 @@ from .utils import get_valid_new_product_info_for_db
 
 
 router = APIRouter(prefix="/products", tags=["Products"])
+
+
+@router.get("/images/{img_title}", status_code=status.HTTP_200_OK)
+async def get_image_test(img_title: str):
+    relative_path = f"product_images/{img_title}"
+    return FileResponse(relative_path)
 
 
 @router.get(
@@ -28,13 +35,40 @@ def get_products(
             models.Product.quantity,
             models.Product.category,
             models.Product.rating,
-            models.ProductImagePath.image_path,
+            models.ProductImageTitle.image_title,
         )
         .outerjoin(
-            models.ProductImagePath,
-            models.Product.id == models.ProductImagePath.product_id,
+            models.ProductImageTitle,
+            models.Product.id == models.ProductImageTitle.product_id,
         )
         .filter(models.Product.category.contains(search.lower()))
+        .limit(limit)
+        .offset(skip)
+    ).all()
+    return products
+
+
+@router.get(
+    "/top-deals",
+    response_model=List[schemas.ProductOut],
+    status_code=status.HTTP_200_OK,
+)
+def get_top_deals(db: Session = Depends(get_db), limit: int = 10, skip: int = 0):
+    products = (
+        db.query(
+            models.Product.id,
+            models.Product.title,
+            models.Product.price,
+            models.Product.quantity,
+            models.Product.category,
+            models.Product.rating,
+            models.ProductImageTitle.image_title,
+        )
+        .outerjoin(
+            models.ProductImageTitle,
+            models.Product.id == models.ProductImageTitle.product_id,
+        )
+        .filter(models.Product.is_top_deal == True)
         .limit(limit)
         .offset(skip)
     ).all()
@@ -64,14 +98,14 @@ def create_product(
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN, detail="Not administrator"
         )
-    new_product, image_path = get_valid_new_product_info_for_db(product)
+    new_product, image_title = get_valid_new_product_info_for_db(product)
     db.add(new_product)
     db.commit()
     db.refresh(new_product)
-    new_image_path = models.ProductImagePath(
-        **{"image_path": image_path, "product_id": new_product.id}
+    image_title = models.ProductImageTitle(
+        **{"image_title": image_title, "product_id": new_product.id}
     )
-    db.add(new_image_path)
+    db.add(image_title)
     db.commit()
-    db.refresh(new_image_path)
+    db.refresh(image_title)
     return new_product
