71996fd7b3915252cbaf9e346dc1c993a51149cf
Forrest Chew <forrestchew@gmail.com>
2022-10-18 15:17:39 -0700

    removed test API endpoint

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from .routers.product import product
 from .routers import auth, user, cart
 
 app = FastAPI()
-
 
 origins = ["http://localhost:3000"]
 
@@ -23,8 +21,3 @@ app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(product.router)
 app.include_router(cart.router)
-
-
-@app.get("/")
-def root():
-    return "Success"
